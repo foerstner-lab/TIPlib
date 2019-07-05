e8eb0b705a649b574e39327dfc92840162a0e3d9
EvaSeidlmayer <eva.seidlmayer@gmx.net>
2019-07-05 17:10:59 +0200

    improvment on test_orcid_input

--- tests/test.py
+++ tests/test.py
@@ -14,125 +14,139 @@ __description__ = 'test of tip-library components'
 
 import pytest
 
-from tip_lib import Authors
-from tip_lib import Institution
-from tip_lib import Journal
-
-'''
-import sys
-print(sys.path)
-sys.path.append('https://github.com/foerstner-lab/TIP-lib/tree/master/tip-lib')
-'''
-
-
+from tip_lib.Authors import Authors
+from tip_lib.Institution import Institution
+from tip_lib.Journal import Journal
 
 '''check the input to get wikidata_IDs'''
 
-def test_orcid_input(self):
-    result = _get_wd_id_by_orcid(Authors(orcid='0000-0003-4619-697X'))
-    assert type(result) == dict
-    assert result == result([' Wikidata-ID'],Q40466811)
+def test_orcid_input():
+    obj = Authors(orcid='0000-0003-4619-697X')
+    result = Authors._get_wd_id_by_orcid(obj)
+    print('result')
+    print(result)
+    assert isinstance(result, dict)
+    assert result['Wikidata-ID'] == 'Q40466811'
+test_orcid_input()
 
 
-def test_isni_input(self):
-    result_Author = _get_wd_id_by_isni(Authors(isni='0000 0001 2117 8244'))
-    result_Institution = _get_wd_id_by_isni(Institution(isni='0000 0001 2165 487X'))
-    assert type(result_Author) == dict
-    assert result_Author == result_Author(['Wikidata-ID'], Q1325)
+
+def test_isni_input():
+    obj_Authors = Authors(isni='0000 0001 2117 8244')
+    result_Authors= obj_Authors._get_wd_id_by_isni()
+    obj_Institution = obj_Institution(isni='0000 0001 2165 487X')
+    result_Institution = obj_Institution._get_wd_id_by_isni()
+    assert type(result_Authors) == dict
+    assert result_Authors == result_Authors(['Wikidata-ID'], 'Q1325')
     assert type(result_Institution) == dict
-    assert result_Institution == result_Institution(['Wikidata-ID'], 546118)
+    assert result_Institution == result_Institution(['Wikidata-ID'], '546118')
+test_isni_input()
 
-
-def test_issn_input(self):
-    result = _get_wd_id_by_issn(Journal(issn='1532-2882'))
+def test_issn_input():
+    obj = Journal(issn='1532-2882')
+    result = obj._get_wd_id_by_issn()
     assert type(result) == dict
-    assert result == result(['Wikidata-ID'], Q152040)
-
+    assert result == result(['Wikidata-ID'], 'Q152040')
+test_issn_input()
 
 
 
 '''check the retrieval of features'''
 
-def test_get_gender(self):
-    result = _get_gender(Authors('Q40904'))
+def test_get_gender():
+    obj = Authors('Q40904')
+    result = obj._get_gender()
     assert type (result) == dict
-    assert result == result(['Gender'], male)
+    assert result == result(['Gender'], 'male')
+test_get_gender()
 
-
-def test_get_affiliation(self):
-    result = _get_affiliation(Authors('Q2395341'))
+def test_get_affiliation():
+    obj = Authors('Q2395341')
+    result = obj._get_affiliation()
     assert type (result) == dict
     assert result == result(['Affiliation'], ['University of Tokyo', 'Kyoto University', 'Osaka University', 'Hirosaki University'])
+test_get_affiliation()
 
-
-def test_get_name(self):
-    result_Authors = _get_name(Authors('Q2395341'))
-    result_Journal = _get_name(Journal('Q180445'))
+def test_get_name():
+    obj_Authors = Authors('Q2395341')
+    obj_Journal = Journal('Q180445')
+    result_Authors = obj_Authors._get_name()
+    result_Journal = obj_Journal._get_name()
     assert type(result_Authors) == dict
     assert result_Authors(['Name'], 'Tasuku Honjo')
     assert type(result_Journal) == dict
     assert result_Journal(['Name'], 'Nature')
- 
+test_get_name()
 
-def test_get_orcid(self):
-    result = _get_orcid(Authors('Q40466811'))
+def test_get_orcid():
+    obj = Authors('Q40466811')
+    result = obj._get_orcid()
     assert type(result) == dict
-    assert result(['ORCID'], Q40466811)
+    assert result(['ORCID'], 'Q40466811')
+test_get_orcid()
 
-
-def test_get_parents(self):
-    result = _get_parents(Authors('Q40904'))
+def test_get_parents():
+    obj = Authors('Q40904')
+    result = obj._get_parents()
     assert type(result) == dict
     assert result(['Parents'], ['August Heisenberg', 'Annie Heisenberg'])
+test_get_parents()
 
-
-def test_get_country(self):
-    result = _get_country(Institution('Q251061'))
+def test_get_country():
+    obj = Institution('Q251061')
+    result = obj._get_country()
     assert type(result) == dict
     assert result(['Country'], 'Germany')
+test_get_country()
 
-
-def test_get_country_of_origin(self):
-    result = _get_country_of_origin(Journal('Q180445'))
+def test_get_country_of_origin():
+    obj = Journal('Q180445')
+    result = obj._get_country_of_origin()
     assert type(result) == dict
     assert result(['Country'], 'United Kingdom')
+test_get_country_of_origin()
 
-
-def test_get_publisher(self):
-    result = _get_publisher(Journal('Q180445'))
+def test_get_publisher():
+    obj = Journal('Q180445')
+    result = obj._get_publisher()
     assert type(result) == dict
     assert result(['Publisher'], 'Nature Publishing Group')
+test_get_publisher()
 
-
-def test_get_review_score(self):
-    result = _get_review_score(Journal('Q180445'))
+def test_get_review_score():
+    obj = Journal('Q180445')
+    result = obj._get_review_score()
     assert type(result) == dict
     assert type(result(['Review_score'], int))
+test_get_review_score()
 
-
-def test_get_main_subject(self):
-    result = _get_main_subject(Journal('Q180445'))
+def test_get_main_subject():
+    obj = Journal('Q180445')
+    result = obj._get_main_subject()
     assert type(result) == dict
     assert result(['Main subjecct'], 'science')
+test_get_main_subject()
 
-
-def test_get_type(self):
-    result = _get_type(Institution('Q251061'))
+def test_get_type():
+    obj = Institution('Q251061')
+    result = obj._get_type()
     assert type(result) == dict
     assert result(['Type'], 'research institute')
+test_get_type()
 
-
-def test_get_students_count(self):
-    result = _get_students_count(Institution('Q251061'))
+def test_get_students_count():
+    obj = Institution('Q251061')
+    result = obj._get_students_count()
     assert type(result) == dict
-    assert result(['Students count'], 11574)
+    assert result(['Students count'], '11574')
+test_get_students_count()
 
-
-def test_get_tuition(self):
-    result = _get_tuition(Institution('Q251061'))
+def test_get_tuition():
+    obj = Institution('Q251061')
+    result = obj._get_tuition()
     assert type(result) == dict
     assert result(['Tuition'], ['54,000,000 Indonesian Rupiah', '19,500,000 Indonesian Rupiah'])
-
+test_get_tuition()
 
 
 
